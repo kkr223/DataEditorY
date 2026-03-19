5d44de0fa19153b5b01a788856751ff5b098c097
kkr223 <564584932@qq.com>
2026-03-19 16:48:11 +0800

    新增规则文本检索

--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -14,6 +14,7 @@ export interface SearchFilters {
   setcode2?: string;
   setcode3?: string;
   setcode4?: string;
+  rule?: string;
 }
 
 export interface SearchFilterState {
@@ -32,6 +33,7 @@ export interface SearchFilterState {
   setcode2: string;
   setcode3: string;
   setcode4: string;
+  rule: string;
 }
 
 export interface SelectOption<T = string | number> {
@@ -68,4 +70,5 @@ export const DEFAULT_SEARCH_FILTERS: SearchFilterState = {
   setcode2: '',
   setcode3: '',
   setcode4: '',
+  rule: '',
 };
